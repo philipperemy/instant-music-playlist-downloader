170c7a34d51f71f1fe86a5fb6ef38ba263db5116
philipperemy <premy@reactive.co.jp>
2018-02-12 15:17:36 +0900

    renaming

--- compress.py
+++ compress.py
@@ -8,9 +8,9 @@ from tqdm import tqdm
 assert len(sys.argv) == 2, 'Please specify the output as parameter.'
 
 INPUT_DIR = sys.argv[1]  # Music dir from download.py script
-OUTPUT_DIR = 'converted_music'
 OUTPUT_BIT_RATE = '192k'
 OUTPUT_FORMAT = 'mp3'
+OUTPUT_DIR = 'converted_music_' + OUTPUT_BIT_RATE + '_' + OUTPUT_FORMAT
 
 if __name__ == '__main__':
 
@@ -18,15 +18,19 @@ if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    musics = glob(INPUT_DIR + '/**/*.mp3', recursive=True)
-    t = tqdm(musics)
+    t = tqdm(glob(INPUT_DIR + '/**/*.mp3', recursive=True))
     for i, music in enumerate(t):
         music_name = os.path.basename(music)
         output_filename = os.path.join(OUTPUT_DIR, music_name)
         t.set_description(music_name)
+
         if not os.path.isfile(output_filename):
             music_fp = AudioSegment.from_file(music)
             # music_fp = music_fp.set_frame_rate(16000)
+
+            if music_fp.duration_seconds > 1000:  # >16min
+                continue
+
             music_fp.export(output_filename,
                             format=OUTPUT_FORMAT,
                             bitrate=OUTPUT_BIT_RATE)
