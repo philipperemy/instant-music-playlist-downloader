6bb60cfbc2db4fca8e870b2ca0c19fa36c7b7c2c
philipperemy <premy@cogent.co.jp>
2018-12-24 14:56:06 +0900

    1

---
+++ list_songs/french_song_listing/clean.py
@@ -0,0 +1,21 @@
+import editdistance
+from tqdm import tqdm
+
+with open('unique.txt', 'r', encoding='utf8') as r:
+    lines = r.read().strip().split('\n')
+
+radius = 20  # already sorted.
+editdistance_discard_threshold = 3
+num_lines = len(lines)
+marked_as_delete = [0] * num_lines
+for i in tqdm(range(num_lines)):
+    for j in range(i, min(i + radius, num_lines)):
+        if i != j and editdistance.eval(lines[i], lines[j]) <= editdistance_discard_threshold:
+            marked_as_delete[j] = 1
+
+with open('unique_2.txt', 'w', encoding='utf8') as w:
+    for k in range(len(marked_as_delete)):
+        if not marked_as_delete[k]:
+            w.write(lines[k])
+            w.write('\n')
+            w.flush()
